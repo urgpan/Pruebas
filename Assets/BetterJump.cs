2ab2bf681dbafbe2367ffe8718c1ea63f25919a6
DESKTOP-4C892Q6\urgpa <urgpan@gmail.com>
2018-01-16 19:33:00 +0100

    Primer COmmit

---
+++ Assets/BetterJump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJump : MonoBehaviour {
+
+    public float multiplicadorCaida = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+    void FixedUpdate()
+    {
+        if (rb.velocity.y < 0)
+            rb.gravityScale = multiplicadorCaida;
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            rb.gravityScale = lowJumpMultiplier;
+        else
+        {
+            rb.gravityScale = 1f;
+        }
+
+
+    }
+	
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float maxSpeed;
+    public float jumpSpeed;
+    bool jumpRequest;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+            jumpRequest = true;
+    }
+    private void FixedUpdate()
+    {
+        Movimiento();
+
+        if(jumpRequest)        
+             Salto();
+    }
+    void Movimiento ()
+    {
+
+        rb.velocity = new Vector2( speed * Input.GetAxis("Horizontal"),rb.velocity.y);
+
+    }
+    void Salto()
+    {       
+        rb.AddForce(Vector2.up * jumpSpeed,ForceMode2D.Impulse);
+        jumpRequest = false;
+
+    }
+}
