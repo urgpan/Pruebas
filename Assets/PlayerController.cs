2b5bbfb331572c56f62a466ce2f7dcddd468fef1
DESKTOP-4C892Q6\urgpa <urgpan@gmail.com>
2018-01-17 15:44:49 +0100

    Salto mejorado

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    //Movimiento y salto
     public float speed;
-    public float maxSpeed;
-    public float jumpSpeed;
+    public float jumpPower;
+
+    //Espacio pulsado
     bool jumpRequest;
 
+    //OnGround
+    public Transform piesPosition;
+    public LayerMask whatsGround;
+    bool onGround;
+
+    //Inercia en el salto
+    float directionInertia;
+    public float inertiaPower;
+
+    //Componentes
     Rigidbody2D rb;
 
     private void Start()
@@ -19,24 +31,42 @@ public class PlayerController : MonoBehaviour {
     {
         if (Input.GetButtonDown("Jump"))
             jumpRequest = true;
+      
     }
     private void FixedUpdate()
     {
+        onGround = Physics2D.OverlapCircle(piesPosition.position, 0.05f, whatsGround);
+        
+
         Movimiento();
 
-        if(jumpRequest)        
-             Salto();
+        if (jumpRequest && onGround)//Si se cumplen los requisitos para saltar: saltar        
+        {
+            Salto();
+            directionInertia = Input.GetAxis("Horizontal");
+        }
+
+        if (!onGround && Input.GetButton("Jump"))
+                Incercia();
+
+
     }
     void Movimiento ()
     {
+        //Movimiento
+        rb.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), rb.velocity.y);
 
-        rb.velocity = new Vector2( speed * Input.GetAxis("Horizontal"),rb.velocity.y);
-
+      
     }
     void Salto()
-    {       
-        rb.AddForce(Vector2.up * jumpSpeed,ForceMode2D.Impulse);
-        jumpRequest = false;
+    {
+       //Impulso hacia arriba
+        rb.AddForce(Vector2.up * jumpPower,ForceMode2D.Impulse);
 
+        jumpRequest = false;
+    }
+    void Incercia()
+    {       //Cuando jump esta pulsado, ejercer una fuerza en la direccion del salto en la horizontal
+            rb.AddForce(Vector2.right * directionInertia * inertiaPower);
     }
 }
